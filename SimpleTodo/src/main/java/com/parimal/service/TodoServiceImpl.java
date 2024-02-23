b0c775f16f1751c18fc5c0abc4dea395c57cd55d
PARIMAL BISWAS <biswasparimal01@gmail.com>
2024-02-23 23:19:21 +0530

    Update TodoServiceImpl.java

--- SimpleTodo/src/main/java/com/parimal/service/TodoServiceImpl.java
+++ SimpleTodo/src/main/java/com/parimal/service/TodoServiceImpl.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.parimal.entity.Todo;
@@ -70,14 +68,7 @@ public class TodoServiceImpl implements TodoService
 	@Override
 	public Page<Todo> getAllTodos(int page, int size, String[] sort, String search)
 	{
-		PageRequest pageRequest = PageRequest.of(page, size, Sort.by(sort));
-		if (search != null && !search.isEmpty())
-		{
-			return todoRepository.findByTitleContainingIgnoreCase(search, pageRequest);
-		} else
-		{
-			return todoRepository.findAll(pageRequest);
-		}
+
 	}
 
 }
